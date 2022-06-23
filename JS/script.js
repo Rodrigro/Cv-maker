e2bffdf6ce286ea7a37ec063dc25831d7d507ad3
Rodrigo Alfonso <rdgdesign@gmail.com>
2022-06-23 14:16:01 -0300

    cv-maker-lucian-v.01

---
+++ JS/script.js
@@ -0,0 +1,21 @@
+let boton = document.querySelector('#boton');
+
+boton.addEventListener('click', ()=>{
+  let contenedor = document.querySelector('#contenido');
+  let div = document.createElement('div');
+  div.innerText = 'Hola, soy un div';
+  contenedor.appendChild(div);
+});
+
+
+function imprimir() {
+	window.print();
+}
+
+function printDiv(nombreDiv) {
+    var contenido= document.getElementById(nombreDiv).innerHTML;
+    var contenidoOriginal= document.body.innerHTML;
+    document.body.innerHTML = contenido;
+    window.print();
+    document.body.innerHTML = contenidoOriginal;
+}
